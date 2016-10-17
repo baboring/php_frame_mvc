1df8182c4361a5db4c862ec12ddc2947e5b80b09
Benjamin <baboring@gmail.com>
2016-10-17 11:01:12 -0400

    add functions  - read safe from (POST,GET)

--- _main/libs/functions.php
+++ _main/libs/functions.php
@@ -112,4 +112,79 @@ function randomPassword($pwd_length) {
 
   return $password;
 }
+
+
+function GetSafeValuePost($arg,$default = null) {
+    if (isset($_POST[$arg])) {
+        // if(is_array($_POST[[$arg]) {
+        //     $arr = array();
+        // }
+        return $_POST[$arg];
+    } 
+    return $default;
+}
+function GetSafeValueGet($arg,$default = null) {
+    if (isset($_GET[$arg])) 
+        return $_GET[$arg];
+    return $default;
+}
+
+// read data from post
+function ReadData_FromPost($lst) {
+    $results = array();
+    foreach($lst as $key=>$value)
+        $results[$value] = GetSafeValuePost($value);
+    //var_dump($results);
+    return $results;
+}
+function ReadData_FromGet($lst) {
+    $results = array();
+    foreach($lst as $key=>$value)
+        $results[$value] = GetSafeValueGet($value);
+    //var_dump($results);
+    return $results;
+}
+
+
+function IsExistVarInPost($val) {
+    return (isset($_POST[$val]) && filter_has_var(INPUT_POST,$val));
+}
+function IsExistVarInGet($val) {
+    return (isset($_GET[$val]) && filter_has_var(INPUT_GET,$val));
+}
+
+
+function GUIDv4 ($trim = true)
+{
+    // Windows
+    if (function_exists('com_create_guid') === true) {
+        if ($trim === true)
+            return trim(com_create_guid(), '{}');
+        else
+            return com_create_guid();
+    }
+
+    // OSX/Linux
+    if (function_exists('openssl_random_pseudo_bytes') === true) {
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    // Fallback (PHP 4.2+)
+    mt_srand((double)microtime() * 10000);
+    $charid = strtolower(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);                  // "-"
+    $lbrace = $trim ? "" : chr(123);    // "{"
+    $rbrace = $trim ? "" : chr(125);    // "}"
+    $guidv4 = $lbrace.
+              substr($charid,  0,  8).$hyphen.
+              substr($charid,  8,  4).$hyphen.
+              substr($charid, 12,  4).$hyphen.
+              substr($charid, 16,  4).$hyphen.
+              substr($charid, 20, 12).
+              $rbrace;
+    return $guidv4;
+}
 ?>
